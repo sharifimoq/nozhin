63e4903fa63fe0caa056360e23f171ad497315ce
sharif <sharifimoq@gmail.com>
2026-06-06 14:44:05 +0330

    Update route.ts

--- src/app/api/admin/products/[id]/route.ts
+++ src/app/api/admin/products/[id]/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = await params;
-  
+  const { id } = await context.params;
+
   await prisma.product.delete({
     where: { id },
   });
