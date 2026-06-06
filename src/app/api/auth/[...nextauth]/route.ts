c7ec0c07f37fc397c1523658edf4aa96e5cd2bf0
sharif <sharifimoq@gmail.com>
2026-06-06 13:59:37 +0330

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,16 @@ import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+    };
+  }
+}
+
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
