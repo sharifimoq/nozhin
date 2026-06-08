4faa63cd5ff966e2af11b362788eaab50c9c3dc6
sharif <sharifimoq@gmail.com>
2026-06-08 11:58:28 +0330

    image

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -1,12 +1,11 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-// افزودن محصول جدید
 export async function POST(req: Request) {
-  const { name, description, price, category, stock, image } = await req.json();
+  const { name, description, price, category, stock, imageUrl } = await req.json();
 
   const product = await prisma.product.create({
-    data: { name, description, price, category, stock, image },
+    data: { name, description, price, category, stock, imageUrl },
   });
 
   return NextResponse.json(product);
